2d3ffbd9d31f8b06f00b3ad9ee81485dfecce84f
Bianca Woo <woobianca@gmail.com>
2017-02-19 22:15:11 -0800

    (develop) Setup app using exponent and add maps with quest markers

--- AnOrcInTheRoad/index.ios.js
+++
@@ -1,5 +0,0 @@
-import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
-import App from './src/components/App/app';
-
-AppRegistry.registerComponent('AnOrcInTheRoad', () => App);
--- AnOrcInTheRoad/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import geolocation from './geolocation';
-
-
-const combinedReducers = combineReducers({
-	coordinates: geolocation
-});
-
-export default combinedReducers;
