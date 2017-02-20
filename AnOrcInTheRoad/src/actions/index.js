3cafeec730cac1d0d333de10b9aafcd0cea3efe8
Bianca Woo <woobianca@gmail.com>
2017-02-19 22:15:11 -0800

    (develop) Setup app using exponent and add maps with quest markers

--- AnOrcInTheRoad/src/actions/index.js
+++
@@ -1,9 +0,0 @@
-let nextTodoId = 0;
-
-export const setCurrentLocation = (coordinates) => {
-	return {
-		type: 'SET_CURRENT_LOCATION',
-		latitude: coordinates.latitude,
-    longitude: coordinates.longitude
-	}
-}
---
+++ app/navigation/Router.js
@@ -0,0 +1,15 @@
+import {
+  createRouter,
+} from '@exponent/ex-navigation';
+
+import HomeScreen from '../screens/HomeScreen';
+import QuestListScreen from '../screens/QuestListScreen';
+import MapScreen from '../screens/MapScreen';
+import RootNavigation from './RootNavigation';
+
+export default createRouter(() => ({
+  home: () => HomeScreen,
+  quests: () => QuestListScreen,
+  map: () => MapScreen,
+  rootNavigation: () => RootNavigation,
+}));
