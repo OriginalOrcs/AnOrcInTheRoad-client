6d325320a21e234e7c758171faf7fe9bf01750c4
Andrew Hsu <andrewhsu42@gmail.com>
2017-02-20 19:07:08 -0800

    Implement QuestCreate

--- app/components/UserProfile.js
+++ app/components/UserProfile.js
@@ -5,14 +5,12 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
     borderWidth: 1,
-    borderColor: '#E7E7E7',
+    borderColor: '#F7F7F7',
     padding: 20,
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'flex-start',
-    marginBottom: 20,
-    marginLeft: 20,
-    marginRight: 20,
+    margin: 20,
   },
   heading: {
     fontSize: 30,
