4b3a89b091b158fee69337dfa078a8a60cb06330
Bianca Woo <woobianca@gmail.com>
2017-02-22 15:55:33 -0800

    (dev) Implement Auth0 Please enter the commit message for your changes. Lines starting

--- app/main.js
+++ app/main.js
@@ -6,6 +6,9 @@ import {
   StatusBar,
   StyleSheet,
   View,
+  Linking,
+  Text,
+  Button,
 } from 'react-native';
 import { createStore, applyMiddleware } from 'redux';
 import {
@@ -27,13 +30,64 @@ import io from 'socket.io-client';
 let socket = io('http://10.7.24.229:3000');
 const store = createStore(reducers);
 
+import jwtDecoder from 'jwt-decode';
+
+let redirectUri;
+if (Exponent.Constants.manifest.xde) {
+  redirectUri = `exp://u3-8hi.woobianca.app.exp.direct/+/redirect`;
+} else {
+  redirectUri = `${Exponent.Constants.linkingUri}/redirect`;
+}
+
+const auth0ClientId = 'vDeBBemEERpMdpAG24zlAdIg2CCIWiQ2';
+const auth0Domain = 'https://originalorcs.auth0.com';
+
 class AppContainer extends React.Component {
   state = {
-    appIsReady: false,
+    appIsReady: false, 
+    username: undefined,
+  }
+
+  componentDidMount() {
+    Linking.addEventListener('url', this._handleAuth0Redirect);
   }
 
   componentWillMount() {
     this._loadAssetsAsync();
+  }
+
+  _loginWithAuth0 = async () => {
+    const redirectionURL = `${auth0Domain}/authorize` + this._toQueryString({
+      client_id: auth0ClientId,
+      response_type: 'token',
+      scope: 'openid name',
+      redirect_uri: redirectUri,
+      state: redirectUri,
+    });
+    Exponent.WebBrowser.openBrowserAsync(redirectionURL);
+  }
+
+  _handleAuth0Redirect = async (event) => {
+    if (!event.url.includes('+/redirect')) {
+      return;
+    }
+    Exponent.WebBrowser.dismissBrowser();
+    const [, queryString] = event.url.split('#');
+    const responseObj = queryString.split('&').reduce((map, pair) => {
+      const [key, value] = pair.split('=');
+      map[key] = value; // eslint-disable-line
+      return map;
+    }, {});
+    const encodedToken = responseObj.id_token;
+    const decodedToken = jwtDecoder(encodedToken);
+    const username = decodedToken.name;
+    this.setState({ username });
+  }
+
+  _toQueryString(params) {
+    return '?' + Object.entries(params)
+      .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+      .join('&');
   }
 
   async _loadAssetsAsync() {
@@ -60,7 +114,14 @@ class AppContainer extends React.Component {
   }
 
   render() {
-    if (this.state.appIsReady) {
+    if (!this.state.username) {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.title}>Example: Auth0 login</Text>
+          <Button title="Login with Auth0" onPress={this._loginWithAuth0} />
+        </View>
+      )
+    } else if (this.state.appIsReady) {
       return (
         <Provider store={store}>
           <View style={styles.container}>
@@ -85,6 +146,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 40,
   },
   statusBarUnderlay: {
     height: 24,
