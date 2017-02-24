16810abc318b03a227955a4963715f6558e35a7c
Bianca Woo <woobianca@gmail.com>
2017-02-23 21:07:03 -0800

    (develop) Fix unmerged main and reducer file changes

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import quests from './quests';
+import auth from './auth';
 
 const App = combineReducers({
   quests: quests,
+  auth: auth,
 });
 
 export default App;
