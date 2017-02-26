f6fae09c390a900da6ed9f47535c07c37a4e5c9b
Bianca Woo <woobianca@gmail.com>
2017-02-25 16:04:42 -0800

    (develop) Add styling to login screen and gifs

--- app/socket/socket.js
+++ app/socket/socket.js
@@ -5,6 +5,10 @@ import { store } from '../main';
 
 const socket = io('10.7.37.58:3000');
 
+// const socket = io('http://10.7.24.210:3000');
+const socket = io('http://169.254.86.190:3000')
+
+
 socket.on('trigger update quests', () => {
   console.log('trigger');
   socket.emit('get quests', 0);
