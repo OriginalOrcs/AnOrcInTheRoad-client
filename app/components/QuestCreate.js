dab86c4ddbbee819e89003b199cd7ff3874c1383
Andrew Hsu <andrewhsu42@gmail.com>
2017-03-09 02:44:47 -0800

    (develop) Implement quest creation for all new quest types

--- app/components/QuestCreate.js
+++ app/components/QuestCreate.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Modal, TouchableHighlight, TextInput, Slider, Picker, ScrollView, Image } from 'react-native';
-import CreateQuestMap from '../screens/MapScreen';
+import { View, Text, StyleSheet, Modal, TouchableHighlight, TextInput, Slider, Picker, ScrollView, Image, TouchableOpacity, DatePickerIOS } from 'react-native';
 import { Font } from 'exponent';
 import MapCreate from './MapQuest';
 import Layout from '../constants/Layout';
@@ -115,8 +114,6 @@ const styles = StyleSheet.create({
   picker: {
     paddingLeft: 50,
     paddingRight: 50,
-    paddingTop: 5,
-    paddingBottom: 5,
   },
   map: {
     backgroundColor: '#fff',
@@ -127,12 +124,45 @@ const styles = StyleSheet.create({
     flex: 1,
     resizeMode: 'cover',
     alignSelf: 'center',
-    padding: 35,
+    // padding: 35,
   },
   slider: {
     width: Layout.window.width - 30,
     alignSelf: 'center',
-  }
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    marginTop: 15,
+  },
+  filterButton: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eee',
+    padding: 10,
+  },
+  middleButton: {
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: '#ccc',
+  },
+  pickerContainer: {
+    // marginTop: 35,
+    height: 300,
+    // overflow: 'hidden',
+    flex: 1,
+    // justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  footer: {
+    // paddingTop: 35,
+    paddingLeft: 35,
+    paddingRight: 35,
+    // paddingBottom: 20,
+    marginBottom: 20,
+    marginTop: 180,
+  },
 });
 
 class QuestCreate extends React.Component {
@@ -140,24 +170,104 @@ class QuestCreate extends React.Component {
     super(props);
     this.state = {
       modalVisible: false,
+      pickerVisible: true,
+      nextModalVisible: true,
       name: null,
       location: null,
       experience: null,
-      questType: 'addFetchQuest',
+      questType: null,
       item_id: null,
       creator_id: null,
       lat: null,
       lng: null,
+      cryptoMessage: "",
+      startDate: new Date,
+      endDate: new Date,
+      showStartTime: true,
+      showEndTime: false,
+      detailsVisible: false,
     };
-  }
-  setModalVisible(visible) {
-    this.setState({modalVisible: visible});
+    this.setModalVisible = this.setModalVisible.bind(this);
+    this.setDetailsVisible = this.setDetailsVisible.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.onRegionChange = this.onRegionChange.bind(this);
+    this.setStartDate = this.setStartDate.bind(this);
+    this.setEndDate = this.setEndDate.bind(this);
   }
 
   onRegionChange(coords) {
+    console.log(coords);
     this.setState({
       lat: coords.latitude,
       lng: coords.longitude,
+    });
+  }
+  
+  setModalVisible(visible) {
+    this.setState({
+      modalVisible: visible,
+    });
+  }
+
+
+  setDetailsVisible(visible) {
+    this.setState({
+      detailsVisible: visible,
+    });
+  }
+
+  setSundialFilter(startOrEnd) {
+    if (startOrEnd === 'start') {
+      this.setState({
+        showStartTime: true,
+        showEndTime: false,
+      });
+    } else {
+      this.setState({
+        showStartTime: false,
+        showEndTime: true,
+      });
+    }
+  }
+
+  createQuest() {
+    const timestamp = Date.now();
+    this.props.onSubmitQuest(
+      this.state.name,
+      this.props.user.char_id,
+      this.state.lat,
+      this.state.lng,
+      this.state.cryptoMessage,
+      this.props.lat,
+      this.props.lng,
+      timestamp,
+      this.state.startDate.getTime(),
+      this.state.endDate.getTime(),
+      this.state.questType,
+    );
+    // this.props.setDetailsVisible(false);
+    // this.props.setModalVisible(false);
+    this.setState({
+      detailsVisible: false,
+      modalVisible: false,
+    });
+  }
+
+  setStartDate(date) {
+    this.setState({
+      startDate: date,
+    });
+  }
+
+  setEndDate(date) {
+    this.setState({
+      endDate: date,
+    });
+  }
+
+  closeModal() {
+    this.setState({
+      modalVisible: false,
     });
   }
 
@@ -169,97 +279,133 @@ class QuestCreate extends React.Component {
           animationType="slide"
           transparent={false}
           visible={this.state.modalVisible}
-          onRequestClose={() => { alert("Modal has been closed.") }}
           style={styles.modal}
         >
-        <MapCreate style={styles.map} onRegionChange={coords => this.onRegionChange(coords)} />
-        <Image style={styles.scroll} source={require('../assets/images/quest-create.png')} >
-          <ScrollView contentContainerStyle={styles.modal}>
-            <TextInput
-              style={styles.input}
-              onChangeText={(name) => this.setState({ name })}
-              placeholder="Quest Name"
-              value={this.state.name}
-              maxLength = {20}
-              autoCorrect = {false}
-              returnKeyType = {'done'}
-            />
-          {/*}  <TextInput
-              style={styles.input}
-              onChangeText={(location) => this.setState({ location })}
-              placeholder="Location"
-              value={this.state.location}
-              maxLength = {150}
-              autoCorrect = {false}
-              returnKeyType = {'done'}
-            /> */}
-            <Text style={styles.label}>Experience: {this.state.experience}</Text>
-            <Slider
-              style={styles.slider}
-              step={10}
-              minimumValue={0}
-              maximumValue={1000}
-              secureTextEntry={true}
-              onValueChange={(value) => {
-                value = Math.floor(value);
-                this.setState({ experience: value });
-              }}
-              onSlidingComplete={(experience) => {
-                experience = Math.floor(experience);
-                this.setState({ experience });
-              }}
-            />
-            {/*} <TextInput
-              style={styles.input}
-              onChangeText={(item_id) => this.setState({ item_id })}
-              placeholder="Item Reward"
-              value={this.state.item_id}
-              maxLength = {60}
-              autoCorrect = {false}
-              returnKeyType = {'done'}
-            /> */}   
-            <Picker
-              selectedValue={this.state.questType}
-              onValueChange={(itemValue) => this.setState({ questType: itemValue })}
-              style={styles.picker}
-            >
-              <Picker.Item label="Fetch Quest" value="addFetchQuest" />
-              <Picker.Item label="Battle - Solo" value="addBattleSoloQuest" />
-              <Picker.Item label="Battle - Co-op" value="addCoopSoloQuest" />
-            </Picker>
-            <TouchableHighlight
-              onPress={() => {
-                this.props.onSubmitQuest(
-                  this.state.name,
-                  this.state.location,
-                  this.state.questType,
-                  this.state.experience,
-                  this.state.lat,
-                  this.state.lng,
-                  this.props.user.char_id,
-                  this.state.item_id,
-                );
-                this.setModalVisible(false);
-              }}
-              style={styles.submitButton}
-            >
-              <Text style={styles.buttonText}>Add Quest</Text>
-            </TouchableHighlight>
-            <TouchableHighlight
-              onPress={() => {
-                this.setModalVisible(!this.state.modalVisible);
-              }}
-              style={styles.closeButton}
-            >
-              <Text style={styles.buttonText}>Cancel</Text>
-            </TouchableHighlight>
-          </ScrollView>
-        </Image> 
+          <MapCreate style={styles.map} onRegionChange={coords => this.onRegionChange(coords)} />
+          <Image style={styles.scroll} source={require('../assets/images/quest-create.png')} >
+            <View style={styles.container}>
+              <View style={styles.pickerContainer} >
+                <Picker
+                  selectedValue={this.state.questType}
+                  onValueChange={(itemValue) => this.setState({ questType: itemValue })}
+                  style={styles.picker}
+                >
+                  <Picker.Item label="Fetch Quest" value="addFetchQuest" />
+                  <Picker.Item label="Crypto Quest" value="addCryptoQuest" />
+                  <Picker.Item label="Sundial Quest" value="addSunDialQuest" />
+                </Picker>
+              </View>
+            </View>
+            <View style={styles.footer}>
+              <TouchableHighlight
+                onPress={() => this.setState({
+                  detailsVisible: true,
+                  modalVisible: false,
+                })}
+                style={styles.submitButton}
+              >
+                <Text style={styles.buttonText}>Next</Text>
+              </TouchableHighlight>
+              <TouchableHighlight
+                onPress={() => {
+                  this.setModalVisible(false);
+                }}
+                style={styles.closeButton}
+              >
+                <Text style={styles.buttonText}>Cancel</Text>
+              </TouchableHighlight>
+            </View>
+          </Image>
+        </Modal>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.detailsVisible}
+          style={styles.modal}
+        >
+          <Image style={styles.scroll} source={require('../assets/images/quest-create.png')} >
+            <View style={styles.container}>
+              <TextInput
+                style={styles.input}
+                onChangeText={(name) => this.setState({ name })}
+                placeholder="Quest Name"
+                value={this.state.name}
+                maxLength = {20}
+                autoCorrect = {false}
+                returnKeyType = {'done'}
+              />
+              {this.state.questType === 'addCryptoQuest' ?
+                <TextInput
+                  style={styles.input}
+                  onChangeText={(cryptoMessage) => this.setState({ cryptoMessage })}
+                  placeholder="Quest Hint"
+                  value={this.state.cryptoMessage}
+                  maxLength = {30}
+                  autoCorrect = {false}
+                  returnKeyType = {'done'}
+                />
+              : null}
+              {this.state.questType === 'addSunDialQuest' ?
+                <View>
+                  <View style={styles.buttonContainer}>
+                    <TouchableOpacity onPress={() => this.setSundialFilter('start')} style={styles.filterButton}>
+                      <Text>Start Time</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => this.setSundialFilter('end')} style={[styles.filterButton, styles.middleButton]}>
+                      <Text>End Time</Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View>
+                    {this.state.showStartTime ?
+                      <View>
+                        <Text>Start Time</Text>
+                        <DatePickerIOS
+                          date={this.state.startDate}
+                          mode="time"
+                          onDateChange={this.setStartDate}
+                          minuteInterval={10}
+                        />
+                      </View>
+                    : null}
+                  </View>
+                  <View>
+                    {this.state.showEndTime ?
+                      <View>
+                        <Text>End Time</Text>
+                        <DatePickerIOS
+                          date={this.state.endDate}
+                          mode="time"
+                          onDateChange={this.setEndDate}
+                          minuteInterval={10}
+                        />
+                      </View>
+                    : null}
+                  </View>
+                </View>
+              : null}
+            </View>
+            <View style={styles.footer}>
+              <TouchableHighlight
+                onPress={() => this.createQuest()}
+                style={styles.submitButton}
+              >
+                <Text style={styles.buttonText}>Submit Quest</Text>
+              </TouchableHighlight>
+              <TouchableHighlight
+                onPress={() => {
+                  this.setState({detailsVisible: false});
+                }}
+                style={styles.closeButton}
+              >
+                <Text style={styles.buttonText}>Cancel</Text>
+              </TouchableHighlight>
+            </View>
+          </Image>
         </Modal>
         <View>
           <TouchableHighlight
             onPress={() => {
-              this.setModalVisible(true);
+              this.setState({ modalVisible: true });
             }}
             style={styles.addButton}
           >
